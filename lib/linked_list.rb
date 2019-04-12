d29516f88ccbc5558968d868f4bff62b237b0774
Scott Thomas <smthom05@gmail.com>
2019-04-12 11:59:57 -0600

    kinda done with iteration 2

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,6 @@
+require './lib/node'
+require 'pry'
+
 class LinkedList
   attr_reader :head
 
@@ -6,19 +9,34 @@ class LinkedList
   end
 
   def append(new_data)
-    @head = Node.new(new_data)
+    if @head != nil
+      find_last_node.next_node = Node.new(new_data)
+    else
+      @head = Node.new(new_data)
+    end
+  end
+
+  def find_last_node(node= @head)
+    return node if !node.next_node
+    find_last_node(node.next_node)
   end
 
   def count
     [@head.data].count
   end
 
+  # Need to fix this logic to iterate over more than two nodes
   def to_string
-    @head.data.to_s
+    node = @head
+    if node.next_node == nil
+      node.data
+    else
+      node.data + " " + node.next_node.data
+    end
   end
-
 end
 
 require './lib/linked_list'
-require './lib/node'
 list = LinkedList.new
+list.append('doop')
+list.append('deep')
